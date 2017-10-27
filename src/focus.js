313a9262f83ac6e5da754b89f52caff6200eee3b
Andrew Lee <andlee@linkedin.com>
2017-10-26 23:42:20 -0700

    Base implementation

--- src/focus.js
+++ src/focus.js
@@ -1,4 +1,10 @@
 (function() {
   'use strict';
-  
+
+  var style = document.createElement('style'),
+      sheet;
+
+  document.head.appendChild(style);
+  sheet = style.sheet;
+  sheet.insertRule('*:focus{outline:1px solid cyan!important;}');
 })();

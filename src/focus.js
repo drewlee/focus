9259ee281a48db2246f4acab9125f13217266cfc
Andrew A Lee <starsthatfell@gmail.com>
2019-10-14 10:59:11 -0700

    Removes overlay in favor of console logging

--- src/focus.js
+++ src/focus.js
@@ -1,20 +1,12 @@
 (function() {
   var style = document.createElement('style'),
-      container = document.createElement('div'),
       sheet;
 
   document.head.appendChild(style);
   sheet = style.sheet;
   sheet.insertRule('*:focus{outline:3px solid cyan!important}');
-  sheet.insertRule('html{margin-top:50px}');
-  sheet.insertRule('.topper{height:50px;width:100%;position:fixed;z-index:2147483647;top:0;background-color:black;color:white}');
 
-  container.className = 'topper';
-  document.body.appendChild(container);
-
-  document.addEventListener('focusout', function(evt) {
-    setTimeout(function() {
-      container.innerText = document.activeElement;
-    }, 100);
+  document.addEventListener('focusout', function() {
+    console.log(document.activeElement);
   });
 })();
